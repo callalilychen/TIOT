3ef7a62e1c605320edb7b2721678072993123ba5
wenwen <wenwen@wwad.de>
2015-08-25 14:51:36 +0200

    begin with hmac

---
+++ crypto/hmac.h
@@ -0,0 +1,14 @@
+#ifndef __HMAC_H__
+#define __HMAC_H__
+
+#include <stdlib.h>
+#ifdef  __cplusplus
+extern "C" {
+#endif
+int hmac()
+#ifdef  __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __HMAC_H__ */
+
