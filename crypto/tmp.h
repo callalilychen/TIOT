f5a00aa7b6fdbb4e71ac18fdd00dba869084da92
wenwen <wenwen@wwad.de>
2015-11-09 13:20:35 +0100

    commit

---
+++ crypto/tmp.h
@@ -0,0 +1,15 @@
+#ifndef __TMP_H__
+#define __TMP_H__
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+#define RIOT_FILE_RELATIVE ""
+#define  DEBUG_FUNC ""
+#ifdef  __cplusplus
+  }
+#endif /* __cplusplus */
+
+#endif /* __TMP_H__ */
