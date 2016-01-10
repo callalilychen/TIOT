b11690eb4642812f4b7ea3507756d6dde4f18ec5
wenwen <wenwen@wwad.de>
2016-01-10 23:53:48 +0100

    comit

--- msp430f5529lp/mutex.h
+++
@@ -1,5 +0,0 @@
-#include <msp430.h>
-#define ENTER_MUTEXT uint16_t srGIEstate = __get_SR_register() & GIE;\
-    __bic_SR_register(GIE);
- 
-#define EXIT_MUTEX __bis_SR_register(srGIEstate);
--- msp430f5529lp/wdt.h
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-#include "treeconfig.h"
-extern volatile uint16_t wdt_counter;
-
-void initWDT(uint16_t);
